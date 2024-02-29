7a657add8b1ae33c0565c6d03f370f95df2bcb3d
aguradev <wahmayun.wm@gmail.com>
2024-02-29 22:41:37 +0800

    update image directory

--- src/contents/categories_list.js
+++ src/contents/categories_list.js
@@ -1,25 +1,25 @@
 export default [
   {
     id: 1,
-    imgUrl: "./src/assets/img/categories-1.jpg",
+    imgUrl: `src/assets/img/categories-1.jpg`,
     title: "Mobile kit UI",
     item_count: 678,
   },
   {
     id: 2,
-    imgUrl: "./src/assets/img/categories-2.jpg",
+    imgUrl: "src/assets/img/categories-2.jpg",
     title: "Fonts",
     item_count: 777,
   },
   {
     id: 3,
-    imgUrl: "./src/assets/img/categories-3.jpg",
+    imgUrl: "src/assets/img/categories-3.jpg",
     title: "Icon Set",
     item_count: 382,
   },
   {
     id: 4,
-    imgUrl: "./src/assets/img/categories-4.jpg",
+    imgUrl: "src/assets/img/categories-4.jpg",
     title: "Website UI kit",
     item_count: 423,
   },
