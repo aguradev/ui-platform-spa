39a148d81f753b976e5d75f78dcbe161e4f77934
aguradev <wahmayun.wm@gmail.com>
2024-04-03 23:08:10 +0800

    add env vite vercel

--- src/components/Navbar/InlineNavbar.vue
+++ src/components/Navbar/InlineNavbar.vue
@@ -18,7 +18,7 @@ const props = defineProps({
 }
 
 .navbar {
-    @apply text-white bg-gray-900 fixed top-0 z-[999] w-full md:relative md:bg-transparent md:text-gray-900 max-w-[1440px] flex items-center justify-around mx-auto py-9 px-9;
+    @apply text-white bg-gray-900 fixed top-0 z-[999] w-full md:relative md:bg-transparent md:text-gray-900 lg:max-w-[1600px] flex items-center justify-between lg:justify-around mx-auto py-9 px-9;
 }
 
 .navbar .wrapper {
